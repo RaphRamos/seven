a5299bc28f72f9b066c9c21893bf3bfd04adf30d
RaphRamos <raphael.au@icloud.com>
2018-07-16 21:03:45 +0800

    Add appointment; Add TnCs; Copy style

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -2,20 +2,21 @@ class EventController < ApplicationController
 
   def new
     @event = Event.new
+    @event.client = Client.new
     @agents = Agent.all
     @eventTypes = EventType.all
+    @appointments = Appointment.all
   end
 
   def create
-    event = Event.new(event_params)
-    event.client.build
-    event.save!
-    render :new
+    event = Event.create!(event_params)
+    redirect_to new_event_path
   end
 
   private
     def event_params
-      params.require(:event).permit(:agent_id, :event_type_id, :start, :end,
-        client: [:location, :name, :email, :phone])
+      params.require(:event).permit(:agent_id, :event_type_id, :appointment_id,
+        :start, :end, :terms_of_service,
+        client_attributes: [:location, :name, :email, :phone])
     end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,24 @@
 class Event < ApplicationRecord
   has_paper_trail
-  
+
   belongs_to :agent
   belongs_to :event_type
   belongs_to :appointment
   belongs_to :client
+
+  accepts_nested_attributes_for :client
+
+  validates :terms_of_service, acceptance: true, presence: true
+
+  def autosave_associated_records_for_client
+    c = Client.arel_table
+    new_client = Client.where(email: client.email).or(
+                  Client.where(c[:name].matches("%#{client.name}%")))
+    if new_client.size == 0
+      self.client.save!
+      self.client_id = self.client.id
+    else
+      self.client = new_client.first
+    end
+  end
 end
