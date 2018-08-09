5df4d7809443a2a89b0c418fd1fd5482a6d3dcca
Raphael Ramos <raphael.ramos@agworld.com.au>
2018-08-09 10:09:54 +0800

    Adding sign-up back

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/administration', as: 'rails_admin'
-  devise_for :admins, skip: [:registrations]
+  devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "event#new"
   resources :event, only: [:new, :create] do
