f0704c29044a5a8e191239b7faebd03bd4a87ba7
Raphael Ramos <raphael.santos.ramos@gmail.com>
2018-10-06 08:45:08 +0800

    Fixing calendar colours and other bugs

--- app/models/timetable.rb
+++ app/models/timetable.rb
@@ -27,6 +27,61 @@ class Timetable < ApplicationRecord
     end
   end
 
+  def self.build_busy_slots(day, agent_id)
+    events = Event.where('events.start < ? AND events.end >= ?', day.beginning_of_day, day.beginning_of_day)
+                  .or(Event.where(start: day.beginning_of_day..day.end_of_day))
+                  .or(Event.where(end: day.beginning_of_day..day.end_of_day))
+                  .where(agent_id: agent_id)
+                  .order(:start)
+
+    busy_slots = []
+
+    events.map do |event|
+      first = '0:00'
+      last = '23:59'
+
+      if event.start.between?(day.beginning_of_day, day.end_of_day)
+        first = event.start.strftime('%R')
+      end
+
+      if event.end.between?(day.beginning_of_day, day.end_of_day)
+        last = event.end.strftime('%R')
+      end
+
+      start_time = first.to_time
+      loop do
+        # busy_slots << start_time.strftime('%R')
+        busy_slots << [start_time.strftime('%R'), (start_time + 30.minutes).strftime('%R')]
+        start_time += 30.minutes
+        break if start_time >= last.to_time
+      end
+    end
+
+    busy_slots.uniq
+  end
+
+  def self.build_available_slots(day, agent_id, event_type_id)
+    slots = []
+    timetables = Timetable.joins(:event_types)
+                          .where(agent_id: agent_id,
+                                 event_types: { id: event_type_id })
+                          .select { |tt| tt.dow.split(',').include?(day.wday.to_s) }
+    return slots if timetables.empty?
+
+    busy_slots = build_busy_slots(day, agent_id)
+    timetables.each do |timetable|
+      start_time = timetable.start_time
+
+      loop do
+        slots << [start_time.strftime('%R'), (start_time + 30.minutes).strftime('%R')] unless busy_slots.include?(start_time.strftime('%R'))
+        start_time += 30.minutes
+        break if start_time >= timetable.end_time
+      end
+    end
+
+    slots
+  end
+
   private
 
   # Rails Admin esta repassando a hora da tela como se fosse UTC, removendo 8 horas fica horario de Perth
@@ -34,4 +89,12 @@ class Timetable < ApplicationRecord
     self.start_time -= 8.hours
     self.end_time -= 8.hours
   end
+
+  # recebe o dia da semana
+  # veja eventos no dia (para o agente)
+  # converte events em timeslots
+  # gera timeslots do timetable
+  # remove slots de eventos
+  # gera o DOW para a data recebida
+
 end
