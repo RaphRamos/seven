1435c448444f5278bb794499605d655968eec351
RaphRamos <raphael.santos.ramos@gmail.com>
2019-09-14 16:31:42 +0800

    layout changes

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,18 +1,18 @@
 class Location < ApplicationRecord
   has_paper_trail
 
-  has_many :agent_locations
+  has_many :agent_locations, dependent: :destroy
   has_many :agents, through: :agent_locations
 
   scope :active, -> { where(active: true) }
-  # scope :available, -> { joins(:agents).merge(Agent.active).uniq }
+  scope :available, -> { joins(:agents).merge(Agent.active).order(name: :asc).uniq }
 
-  def self.available(is_offshore)
-    locations = if is_offshore
-                  Location.where(name: 'Other')
-                else
-                  Location.where.not(name: 'Other')
-                end
-    locations.joins(:agents).merge(Agent.active).order(name: :asc).uniq
-  end
+  # def self.available(is_offshore)
+  #   locations = if is_offshore
+  #                 Location.where(name: 'Other')
+  #               else
+  #                 Location.where.not(name: 'Other')
+  #               end
+  #   locations.joins(:agents).merge(Agent.active).order(name: :asc).uniq
+  # end
 end
