4dadef64b04bf05d7aabbb3eb00406741a663971
RaphRamos <raphael.santos.ramos@gmail.com>
2019-11-23 20:12:29 +0800

    Allow all requests

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,9 @@ module SevenMigration
         resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
+
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'ALLOWALL'
+    }
   end
 end
